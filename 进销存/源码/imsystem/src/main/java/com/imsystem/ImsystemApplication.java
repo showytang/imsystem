37f560c434e9441c034e103ee2587e5a9173aafa
showytang <35807475+showytang@users.noreply.github.com>
2019-03-15 11:11:36 +0800

    初始化

--- 进销存/源码/imsystem/src/main/java/com/imsystem/ImsystemApplication.java
+++ 进销存/源码/imsystem/src/main/java/com/imsystem/ImsystemApplication.java
@@ -1,9 +1,11 @@
 package com.imsystem;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.imsystem.mapper")
 public class ImsystemApplication {
 
 	public static void main(String[] args) {
