40adf7451df4d0aaab7103721a86f0b2904c7281
chenzhixuan <czx_ivy5869@163.com>
2019-03-30 14:05:36 +0800

    czx创建package

--- 进销存/源码/imsystem/src/main/java/com/imsystem/controller/TestController.java
+++ 进销存/源码/imsystem/src/main/java/com/imsystem/controller/TestController.java
@@ -6,8 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("all")
 public class TestController {
-	@RequestMapping("test")
-	public String test() {
+	@RequestMapping("page-login")
+	public String login() {
 		return "czx/page-login";
+	}
+	@RequestMapping("admin-list")
+	public String XiTonglist() {
+		return "czx/admin-list";
+	}
+	@RequestMapping("admin-add")
+	public String XiTongadd() {
+		return "czx/admin-add";
+	}
+	@RequestMapping("admin-edit")
+	public String XiTongedit() {
+		return "czx/admin-edit";
 	}
 }
