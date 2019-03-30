bfeaac2640d0256b24c6568bc151582df05a2c22
SupperGoodMan <3308704587@qq.com>
2019-03-30 10:28:28 +0800

    2019-3-30-10-28

--- 进销存/源码/imsystem/src/main/java/com/imsystem/controller/order/OrderController.java
+++ 进销存/源码/imsystem/src/main/java/com/imsystem/controller/order/OrderController.java
@@ -1,8 +1,10 @@
 package com.imsystem.controller.order;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/order")
+@Controller
 public class OrderController {
 
 	@RequestMapping("/show")
