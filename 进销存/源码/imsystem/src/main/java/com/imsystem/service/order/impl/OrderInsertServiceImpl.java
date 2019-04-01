f5d3f3fc760eee7cdb34fa60b3ac4b2d9bb214c6
SupperGoodMan <3308704587@qq.com>
2019-04-01 08:46:29 +0800

    创建service层ok

--- 进销存/源码/imsystem/src/main/java/com/imsystem/service/order/impl/OrderInsertServiceImpl.java
+++ 进销存/源码/imsystem/src/main/java/com/imsystem/service/order/impl/OrderInsertServiceImpl.java
@@ -1,4 +1,4 @@
-package com.imsystem.service.impl.orderImpl;
+package com.imsystem.service.order.impl;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,5 +8,5 @@ import com.imsystem.service.order.OrderInsertService;
 @Service
 @Transactional
 public class OrderInsertServiceImpl implements OrderInsertService{
-
+	
 }
