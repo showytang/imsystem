57d103d172103a353db59a7512188adddfd5a750
DavidLikeBanana <1175063903@qq.com>
2019-05-14 17:16:28 +0800

    00

--- 进销存/源码/imsystem/src/main/java/com/imsystem/controller/promotion/PromotionController.java
+++ 进销存/源码/imsystem/src/main/java/com/imsystem/controller/promotion/PromotionController.java
@@ -1,10 +1,15 @@
 package com.imsystem.controller.promotion;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.imsystem.domain.Goods;
+import com.imsystem.domain.Promotion;
 import com.imsystem.service.promotion.PromotionMapService;
 
 /**
@@ -19,6 +24,11 @@ public class PromotionController {
 	@Autowired
 	private PromotionMapService promapSer;
 	
+	/**
+	 * 促销管理页面
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping("/toPromotion")
 	public String toPromotion(Model model) {
 		
@@ -26,6 +36,23 @@ public class PromotionController {
 		
 		return "dws/promotion";
 	}
+	
+	
+	/**
+	 * 促销新增
+	 * @param proLis
+	 * @param list
+	 * @return
+	 */
+	@RequestMapping("/inertPromotion")
+	@ResponseBody
+	public int inertPromotion(List<Promotion> proLis,List<Goods> list) {
+		
+		return 0;
+		
+	}
+	
+	
 	
 	
 }
